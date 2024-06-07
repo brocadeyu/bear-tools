88e33a2cf0dbc8186ead45201e0ed6cb16601ef6
brocadeyu <yjb1725594062@gmail.com>
2024-06-07 14:57:13 +0800

    feat: 修正vh

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'normalize.css/normalize.css'
+import VhCheck from 'vh-check'
+VhCheck()
 createApp(App).use(router).mount('#app')
